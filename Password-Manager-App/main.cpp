b4406525c81e9760f0cb67ca6ff1c8bf192d29f0
csabimvp <kellercsabii@gmail.com>
2025-08-01 11:26:46 +0100

    Initial Commit

---
+++ Password-Manager-App/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Password-Manager-App
+//
+//  Created by Csaba Keller on 01/08/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
